f0ce005baa96affe3563e3f0549cb8592d5e3a1f
Adi161201 <mehrotraadityas@gmail.com>
2022-08-03 23:34:53 +0530

    latest

---
+++ Task-4/SwapCase.py
@@ -0,0 +1,6 @@
+s= "Aditya.COM2 Mehrotra.com is2"
+b=""
+for i in range(len(s)):
+    b=b + s[i].lower()
+
+print(b)
---
+++ Task-4/tempCodeRunnerFile.py
@@ -0,0 +1,5 @@
+ans=""
+a='Aditya Mehrotra is a good boy'
+ans=ans.split(" ").join("-")
+
+print(ans)
