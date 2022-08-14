3f23116c6c60e68dbaa402529ab2580f635ee51c
Adi161201 <mehrotraadityas@gmail.com>
2022-08-14 12:39:36 +0530

    new

---
+++ Task-9/app.py
@@ -0,0 +1,39 @@
+#1
+from flask import Flask, render_template, request 
+import re
+
+name=""
+reg=""
+
+#2
+app=Flask(__name__)
+
+#3
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/search', methods=['GET','POST'])
+def search():
+    global name,reg
+    if request.method=='POST':
+        name=request.form["in_1"]
+        reg=request.form["in_2"]
+
+        pattern = re.compile(reg)
+        
+        total = len(pattern.findall(name))
+        matched = pattern.findall(name)
+        
+
+       
+        return render_template('search.html',a=total, b=matched, c=reg, d=name)
+    return render_template('index.html')
+    
+
+
+
+#4
+if __name__=='__main__':
+    app.run(debug=True)
+
